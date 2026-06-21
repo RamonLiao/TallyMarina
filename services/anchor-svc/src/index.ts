e97df047f13efe0958c3c8e34cc2321d0942763c
ramonliao <yc52811@hotmail.com>
2026-06-22 03:00:27 +0800

    fix(anchor-svc): re-export deriveEntityRef from index barrel (Task 8 monkey imports it)

--- services/anchor-svc/src/index.ts
+++ services/anchor-svc/src/index.ts
@@ -1,4 +1,5 @@
 export * from './domain/types.js';
+export { deriveEntityRef } from './core/entityRef.js';
 export { lookupEntity } from './core/registry.js';
 export { resolveChain, type ResolvedChain } from './core/resolveChain.js';
 export { buildAnchorArgs, type AnchorPayloadInput } from './core/buildAnchorArgs.js';
